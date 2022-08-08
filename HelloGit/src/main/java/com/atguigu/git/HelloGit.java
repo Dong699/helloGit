62735544cd8271e746ffcd0414cd49d081e0127d
Dong699 <2175298805@qq.com>
2022-08-08 13:27:02 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @date 2022-08-08 13:01
+ */
+public class HelloGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
